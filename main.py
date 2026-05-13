76d59ee74238100470c9f396f661b09787ae076c
Wayan Ardike <113248931+WayanArdike@users.noreply.github.com>
2026-05-13 08:09:41 +0700

    update v10

--- main.py
+++ main.py
@@ -27,10 +27,7 @@ app = FastAPI(title="Sistem Perpustakaan API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "http://127.0.0.1:3000"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
